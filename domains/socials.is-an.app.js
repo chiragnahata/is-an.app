469e6a3052f39ea493867e25a2d820dcd088cfa9
Chirag Nahata <chiragnahata05@gmail.com>
2024-03-29 22:43:36 +0530

    Create socials.is-an.app.js

---
+++ domains/socials.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'A social media site',
+  domain: 'is-an.app',
+  subdomain: 'socials',
+  owner: {
+    email: 'chiragnahata05@gmail.com',
+  },
+  record: {
+    A: '129.213.151.29',
+  },
+})
